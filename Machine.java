704ae088acd63bb8c496ce0a553028e6df751ff7
Thomas Schwalen <thomasschwalen@gmail.com>
2017-10-03 20:54:18 -0500

    Add files via upload
    Started to implement registers and the ability to reference the registers and tape as arguments to certain commands. The code is kind of gross right now, so I may refactor and clean it up before I move further.

--- Machine.java
+++ Machine.java
@@ -1,12 +1,20 @@
 public class Machine {
 	
 	final static int INITIAL_TAPE_SIZE = 500;
+	final static int NUMBER_OF_REGISTERS = 8;
 
-	final static int MVUP = 0, MVDN = 1, WRITE = 2, PRINTCHR = 3, PRINTINT = 4;
+	final static int LIT = -2, REG = -1, MVUP = 0, MVDN = 1, WRITE = 2, PRINTCHR = 3, PRINTINT = 4, LOOP_LIT = 5,
+		LOOP_VAR = 6, JUMP_BACK = 7, MVUX = 8, MVDX = 9, PEEK = 10, SET = 11;
+
+	// memory access variables 
+	final static int RE1 = 0, RE2 = 1, RE3 = 2, RE4 = 3, RE5 = 4, RE6 = 5,
+		RE7 = 6, RE8 = 7;
 
 	int tapeLength;
 	int[] tape;
+	int[] registers;
 	int headPosition;
+	int pc;
 
 	public static void main(String[] args){
 		int[] testPrgm = {
@@ -25,22 +33,44 @@ public class Machine {
 	public Machine(int[] cmds){
 		tapeLength = INITIAL_TAPE_SIZE;
 		tape = new int[INITIAL_TAPE_SIZE];
+		registers = new int[NUMBER_OF_REGISTERS];
 		headPosition = 0;
 		execute(cmds);
 	}
 
 	private void execute(int[] cmds){
-		for(int pc = 0; pc < cmds.length; pc++){
+		// replace with a stack
+		int loopCtrl = 0;
+		int jumpLength = 0;
+		for(pc = 0; pc < cmds.length; pc++){
+
 			switch(cmds[pc]){
+				case SET:
+					// get the register destination
+					pc++;
+					int regNum = cmds[pc];		
+					registers[regNum] = retrieveValueOrLiteral(cmds);
+					break;
+
 				case MVUP:
 					moveUp();
 					break;
 				case MVDN:
 					moveDown();
 					break;
-				case WRITE:
+				case MVUX:
 					pc++;
-					write(cmds[pc]);
+					headPosition += cmds[pc];
+					checkTape();
+					break;
+				case MVDX:
+					pc++;
+					headPosition -= cmds[pc];
+					checkTape();				
+					break;
+				case WRITE:
+					int value = retrieveValueOrLiteral(cmds);
+					write(value);
 					break;
 				case PRINTINT:
 					System.out.print(readInt());
@@ -48,8 +78,43 @@ public class Machine {
 				case PRINTCHR:
 					System.out.print(readChar());
 					break;
+				case LOOP_LIT:						// LOOP <number of iterations>
+					pc++;
+					loopCtrl = cmds[pc];
+					break;
+				case JUMP_BACK:        				// JUMP_BACK <# of lexemes>
+					if(loopCtrl > 1){
+						loopCtrl--;
+						pc -= cmds[pc + 1]; // jump back value
+					}
+					else{
+						pc += 2; // skip the jump back number
+					}
+					break;
+
+
 			}
 		}
+	}
+
+	private int retrieveValueOrLiteral(int[] cmds){
+		pc++;
+		int ctrl = cmds[pc];
+
+		if(ctrl == PEEK){
+			return tape[headPosition];
+		}
+		else{
+			pc++;
+			if(ctrl == REG){
+				return registers[cmds[pc]];
+			}
+			else if(ctrl == LIT){
+				return cmds[pc];
+			}
+		}
+		System.exit(0);
+		return 0x80000000;
 	}
 
 	public int readInt(){
--- Parser.java
+++ Parser.java
@@ -2,10 +2,13 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.LinkedList;
 
+
 public class Parser {
 
-	final static int MVUP = 0, MVDN = 1, WRITE = 2, PRINTCHR = 3, PRINTINT = 4;
+	final static int LIT = -2, REG = -1, MVUP = 0, MVDN = 1, WRITE = 2, PRINTCHR = 3, PRINTINT = 4, LOOP_LIT = 5,
+		LOOP_VAR = 6, JUMP_BACK = 7, MVUX = 8, MVDX = 9, PEEK = 10, SET = 11;
 
+	final static int REG_OFFSET = 49;
 
 	public static void main(String[] args){
 		if(args.length == 0){
@@ -21,35 +24,64 @@ public class Parser {
 			return;
 		}
 
-		Machine runMachine = new Machine(parse(input));
+		int[] result = parse(input);
+		/*
+		for(int i : result){
+			System.out.println(i);
+		}
+		*/
+		Machine runMachine = new Machine(result);
+		//Machine runMachine = new Machine(parse(input));
 	}
 
 	private static int[] parse(Scanner input){
 		LinkedList<Integer> commands = new LinkedList<>();
 
 		String current;
+		boolean inLoop = false;
+		int lengthAtLoopStart = 0;
+
 		while(input.hasNext()){
 			current = input.next();
 			switch(current){
+				case "SET":
+					commands.add(SET);
+
+					String arg = input.next();
+
+					if(arg.substring(0,2).equals("RE")){
+						//commands.add(REG);
+						int regNum = arg.charAt(2) - REG_OFFSET;
+
+						if(!(regNum >= 0 && regNum < 8)){
+							syntaxError("Invalid register");
+						}
+						commands.add(regNum);
+					}
+					
+					determineReference(commands, input);
+					break;
 				case "MVUP":
 					commands.add(MVUP);
 					break;
 				case "MVDN":
 					commands.add(MVDN);
 					break;
-				case "WRITE":
-					if(input.hasNext()){
-						commands.add(WRITE);
-						String arg = input.next();
-
-						// check if it's a character
-						if(arg.length() == 3 && arg.charAt(0) == '<' && arg.charAt(2) == '>'){
-							commands.add((int)arg.charAt(1));
-						}
-						else{
-							commands.add(Integer.parseInt(arg));
-						}
+				case "MVUX":
+					commands.add(MVUX);
+					if(input.hasNextInt()){
+						commands.add(input.nextInt());
 					}
+					break;
+				case "MVDX":
+					commands.add(MVDX);
+					if(input.hasNextInt()){
+						commands.add(input.nextInt());
+					}
+					break;
+				case "WRITE":
+					commands.add(WRITE);
+					determineReference(commands, input);
 					break;
 				case "PRINTCHR":
 					commands.add(PRINTCHR);
@@ -57,12 +89,90 @@ public class Parser {
 				case "PRINTINT":
 					commands.add(PRINTINT);
 					break;
+				case "LOOP":
+					if(inLoop){
+						syntaxError("Nested loops not permitted");
+					}
+
+					commands.add(LOOP_LIT);
+
+					// add number of iterations
+					if(input.hasNextInt()){
+						commands.add(input.nextInt());
+					}
+
+					if(!input.next().equals("{")){
+						syntaxError("Expected {");
+					}
+
+					inLoop = true;
+					lengthAtLoopStart = commands.size();
+					break;
+
+				case "}":
+					if(inLoop){
+						commands.add(JUMP_BACK);
+						int loopSize = commands.size() - lengthAtLoopStart;
+						commands.add(loopSize);
+					}
+					else{
+						syntaxError("Unexpected Character: }");
+					}
+					break;
+				default:
+					syntaxError("Unexpected or unrecognized lexeme");
 			}
 
 		}
 
 		return toIntArray(commands);
-	} 
+	}
+
+	private static void determineReference(LinkedList<Integer> commands, Scanner input){
+		if(input.hasNext()){
+
+			if(input.hasNextInt()){
+				commands.add(LIT);
+				commands.add(input.nextInt());
+				return;
+			}
+
+			String arg = input.next();
+						
+			// check if it's a peek 
+			if(arg.equals("PEEK")){
+				commands.add(PEEK);
+			}
+			// check if it's a register
+			else if(arg.substring(0,2).equals("RE")){
+				commands.add(REG);
+				int regNum = arg.charAt(2) - REG_OFFSET;
+				if(!(regNum >= 0 && regNum < 8)){
+					syntaxError("Invalid register");
+				}
+				commands.add(regNum);
+			}
+			// check if it's a character literal
+			else if(arg.charAt(0) == '<' && arg.charAt(arg.length() - 1) == '>'){
+				commands.add(LIT);
+				if(arg.length() == 3){
+					commands.add((int)arg.charAt(1));
+				}
+				else{
+					switch(arg.substring(1,arg.length() - 1)){
+						case "\\n":
+							commands.add((int)('\n'));
+							break;
+						case "\\sp":
+							commands.add((int)(' '));
+							break;
+						default:
+							syntaxError("Unknown special sequence");
+					}
+				}			
+			}
+		}
+	}
 
 	private static int[] toIntArray(LinkedList<Integer> list){
 		int[] array = new int[list.size()];
@@ -73,4 +183,10 @@ public class Parser {
 		}
 		return array;
 	}
+
+	private static void syntaxError(String issue){
+		System.out.println("Syntax Error: " + issue);
+		System.exit(0);
+	}
+
 }
